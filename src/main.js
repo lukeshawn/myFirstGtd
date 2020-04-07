89ef023b19f29ab9c59fb349e80ddb47dba80388
lukeshawn <lukeshawn1992@gmail.com>
2020-04-07 21:39:29 +0800

    gtd初步完成

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
-
+import store from './store.js'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 Vue.config.productionTip = false
 
+Vue.use(Antd)
 new Vue({
-  store,
-  render: h => h(App)
+    store,
+    render: h => h(App)
 }).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    chainWebpack: config => {
+        config.module.rules.delete('eslint');
+    },
+    devServer: {
+        open: true,
+        port: 8878,
+        overlay: {
+            warnings: true,
+            errors: true
+        }
+    }
+}
